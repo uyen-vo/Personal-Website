18b423b90106562a93e38484e10c7d2e1a323b37
Uyen Vo <uyenvo@tamu.edu>
2017-03-11 01:30:15 -0600

    Added hover states and fiddled with the jQuery

--- javascript/js.js
+++ javascript/js.js
@@ -6,11 +6,11 @@ window.onload = function(){
 	$("nav ul").find('a').click(function() {
 		var $href = $(this).attr('href');
     	var $anchor;
-    	$("#c").html("creative");
-    	$("#d").html("developer");
+    	$("#c").html("<h3>//creative <img class='blink' src='images/blink.gif'/></h3>");
+    	$("#d").html("<h3>//developer <img class='blink' src='images/blink.gif'/></h3>");
     	if($href == "#about") { $anchor = $anchor1; }
-    	if($href == "#creative") { $anchor = $anchor2; $(this).text("//creative"); }
-    	if($href == "#dev") { $anchor = $anchor3; $(this).text("//developer"); }
+    	if($href == "#creative") { $anchor = $anchor2; $(this).html("<h3>creative <img class='blink' src='images/blink.gif'/></h3>"); }
+    	if($href == "#dev") { $anchor = $anchor3; $(this).html("<h3>developer <img class='blink' src='images/blink.gif'/></h3>"); }
 
    		$('.maincontent').animate({ scrollTop: $anchor.top }, 'slow');
     	return false;
